756718d7072ffad532a761bf43e5277a1c36280b
Robert Zimmerman <feldenthorne@users.sf.net>
2014-09-01 16:48:00 -0700

    Modified header for readability

--- include/gba.h
+++ include/gba.h
@@ -1,32 +1,32 @@
-/*
-
-	Header file for libgba bios affine functions
-
-	Copyright 2003-2005 by Dave Murphy.
-
-	This library is free software; you can redistribute it and/or
-	modify it under the terms of the GNU Library General Public
-	License as published by the Free Software Foundation; either
-	version 2 of the License, or (at your option) any later version.
-
-	This library is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-	Library General Public License for more details.
-
-	You should have received a copy of the GNU Library General Public
-	License along with this library; if not, write to the Free Software
-	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-	USA.
-
-	Please report all bugs and problems through the bug tracker at
-	"http://sourceforge.net/tracker/?group_id=114505&atid=668551".
-
-*/
-//---------------------------------------------------------------------------------
+//**********************************************************************************
+/** \file gba.h
+ *  Header for GBA function includes
+ * 
+ *  This is just the overarching, "include everything" include for the GBA
+ *  libraries.
+ */ 
+ /* Copyright 2003-2005 by Dave Murphy.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA.
+ * 
+ * Please report all bugs and problems through the bug tracker at
+ * "http://sourceforge.net/tracker/?group_id=114505&atid=668551". */
+//*********************************************************************************
 #ifndef	_gba_h_
 #define	_gba_h_
-//---------------------------------------------------------------------------------
 
 #include <gba_affine.h>
 #include <gba_compression.h>
